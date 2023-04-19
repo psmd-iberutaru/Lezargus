9f9ef1b71f8f646a05245d0d78b518b8209fb396
Sparrow <psmd.iberutaru@gmail.com>
2023-04-18 15:53:20 -1000

    Build framework.

--- src/lezargus/__init__.py
+++ src/lezargus/__init__.py
@@ -1,4 +1,4 @@
+"""Lezargus: The software package related to IRTF SPECTRE."""
+
 # SPDX-FileCopyrightText: 2023-present Sparrow <psmd.iberutaru@gmail.com>
 # SPDX-License-Identifier: MIT
-
-print("hello world.")
