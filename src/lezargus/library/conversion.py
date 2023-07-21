d70bea88e057d107a187496f79a4f756d7ccdd52
Sparrow <psmd.iberutaru@gmail.com>
2023-07-20 18:09:24 -1000

    Atmospheric dispersion

---
+++ src/lezargus/library/conversion.py
@@ -0,0 +1,6 @@
+"""Functions to convert things into something else.
+
+Any and all generic conversions (string, units, or otherwise) can be found in 
+here. Extremely standard conversion functions are welcome in here, but, 
+sometimes, a simple multiplication factor is more effective.
+"""
